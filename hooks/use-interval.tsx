5e2b59f02cdc564b13c7fdef2b9a391cd430a89b
Michael Knepprath <mknepprath@gmail.com>
2021-11-04 10:06:00 -0400

    Push progress so far

---
+++ hooks/use-interval.tsx
@@ -0,0 +1,26 @@
+import { useEffect, useLayoutEffect, useRef } from "react";
+
+function useInterval(callback: () => void, delay: number | null) {
+  const savedCallback = useRef(callback);
+
+  // Remember the latest callback if it changes.
+  useLayoutEffect(() => {
+    savedCallback.current = callback;
+  }, [callback]);
+
+  // Set up the interval.
+  useEffect(() => {
+    // Don't schedule if no delay is specified.
+    if (!delay) {
+      return;
+    }
+
+    const id = setInterval(() => savedCallback.current(), delay);
+
+    return () => clearInterval(id);
+  }, [delay]);
+}
+
+export default useInterval;
+
+// https://usehooks-ts.com/react-hook/use-interval
